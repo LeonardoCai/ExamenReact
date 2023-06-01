c92281a86a51d05afd5fe374cc795767e7e4a284
LeonardoCai <leonardo.caihuara@gmail.com>
2023-05-31 21:12:52 -0300

    first commit

---
+++ src/Components/Card.jsx
@@ -0,0 +1,20 @@
+//Este componente deberia recibir por props y mostrar en pantalla la informacion
+//que envia el usuario
+
+import React from 'react'
+
+const Card = ({personajes}) => {
+  
+
+  return (
+    <div>
+      <h3>Su personaje se llama : {personajes.nombre} </h3>
+      <h3>Su poder es:  {personajes.poder}</h3>
+      <h3>Transformacion: {personajes.transformacion}</h3>
+      <h3>Raza: {personajes.raza}</h3>
+    
+    </div>
+  )
+}
+
+export default Card
